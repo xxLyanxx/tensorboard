57d06fa298b58a8afff5adc5a6066c648f54a616
William Chargin <wchargin@gmail.com>
2020-07-29 16:06:02 -0700

    tf_tensorboard: raw codemod
    Plus:
    
    ```
    rsync -r {/tmp/tbmigrate,.}/tensorboard/components_polymer3/tf_tensorboard/
    ```

--- tensorboard/components_polymer3/tf_tensorboard/autoReloadBehavior.ts
+++ tensorboard/components_polymer3/tf_tensorboard/autoReloadBehavior.ts
@@ -12,102 +12,116 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-namespace tf_tensorboard {
-  export var AUTORELOAD_LOCALSTORAGE_KEY = 'TF.TensorBoard.autoReloadEnabled';
+import "@polymer/iron-icons";
+import "@polymer/paper-button";
+import "@polymer/paper-checkbox";
+import "@polymer/paper-dialog";
+import "@polymer/paper-header-panel";
+import "@polymer/paper-listbox";
+import "@polymer/paper-icon-button";
+import "@polymer/paper-tabs";
+import "@polymer/paper-toolbar";
+import { DO_NOT_SUBMIT } from "../tf-imports/polymer.html";
+import { DO_NOT_SUBMIT } from "../tf-backend/tf-backend.html";
+import { DO_NOT_SUBMIT } from "../tf-dashboard-common/tensorboard-color.html";
+import { DO_NOT_SUBMIT } from "../tf-globals/tf-globals.html";
+import { DO_NOT_SUBMIT } from "../tf-imports/lodash.html";
+import { DO_NOT_SUBMIT } from "../tf-paginated-view/tf-paginated-view-store.html";
+import { DO_NOT_SUBMIT } from "../tf-storage/tf-storage.html";
+import { DO_NOT_SUBMIT } from "../tf-plugin-util/plugin-host.html";
+import { DO_NOT_SUBMIT } from "registry.html";
+import { DO_NOT_SUBMIT } from "tf-tensorboard";
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 
-  var getAutoReloadFromLocalStorage: () => boolean = () => {
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+export var AUTORELOAD_LOCALSTORAGE_KEY = "TF.TensorBoard.autoReloadEnabled";
+var getAutoReloadFromLocalStorage: () => boolean = () => {
     var val = window.localStorage.getItem(AUTORELOAD_LOCALSTORAGE_KEY);
-    return val === 'true' || val == null; // defaults to true
-  };
-
-  function forceDisableAutoReload(): boolean {
-    return new URLSearchParams(window.location.search).has(
-      '_DisableAutoReload'
-    );
-  }
-
-  /**
-   * @polymerBehavior
-   */
-  export var AutoReloadBehavior = {
+    return val === "true" || val == null; // defaults to true
+};
+function forceDisableAutoReload(): boolean {
+    return new URLSearchParams(window.location.search).has("_DisableAutoReload");
+}
+/**
+ * @polymerBehavior
+ */
+export var AutoReloadBehavior = {
     properties: {
-      autoReloadEnabled: {
-        type: Boolean,
-        observer: '_autoReloadObserver',
-        value: getAutoReloadFromLocalStorage,
-      },
-      _autoReloadId: {
-        type: Number,
-      },
-      // Tracks whethere an auto reload was missed because the document was not visible.
-      _missedAutoReload: {
-        type: Boolean,
-        value: false,
-      },
-      _boundHandleVisibilityChange: {
-        type: Object,
-      },
-      autoReloadIntervalSecs: {
-        type: Number,
-        value: 30,
-      },
+        autoReloadEnabled: {
+            type: Boolean,
+            observer: "_autoReloadObserver",
+            value: getAutoReloadFromLocalStorage,
+        },
+        _autoReloadId: {
+            type: Number,
+        },
+        // Tracks whethere an auto reload was missed because the document was not visible.
+        _missedAutoReload: {
+            type: Boolean,
+            value: false,
+        },
+        _boundHandleVisibilityChange: {
+            type: Object,
+        },
+        autoReloadIntervalSecs: {
+            type: Number,
+            value: 30,
+        },
     },
-    attached: function() {
-      this._boundHandleVisibilityChange = this._handleVisibilityChange.bind(
-        this
-      );
-      document.addEventListener(
-        'visibilitychange',
-        this._boundHandleVisibilityChange
-      );
+    attached: function () {
+        this._boundHandleVisibilityChange = this._handleVisibilityChange.bind(this);
+        document.addEventListener("visibilitychange", this._boundHandleVisibilityChange);
     },
-    detached: function() {
-      window.clearTimeout(this._autoReloadId);
-      document.removeEventListener(
-        'visibilitychange',
-        this._boundHandleVisibilityChange
-      );
-    },
-    _autoReloadObserver: function(autoReload) {
-      window.localStorage.setItem(AUTORELOAD_LOCALSTORAGE_KEY, autoReload);
-      if (autoReload && !forceDisableAutoReload()) {
-        this._autoReloadId = window.setTimeout(
-          () => this._doAutoReload(),
-          this.autoReloadIntervalSecs * 1000
-        );
-      } else {
+    detached: function () {
         window.clearTimeout(this._autoReloadId);
-      }
+        document.removeEventListener("visibilitychange", this._boundHandleVisibilityChange);
     },
-    _doAutoReload: function() {
-      if (this._isDocumentVisible()) {
-        this._doReload();
-      } else {
-        this._missedAutoReload = true;
-      }
-      this._autoReloadId = window.setTimeout(
-        () => this._doAutoReload(),
-        this.autoReloadIntervalSecs * 1000
-      );
+    _autoReloadObserver: function (autoReload) {
+        window.localStorage.setItem(AUTORELOAD_LOCALSTORAGE_KEY, autoReload);
+        if (autoReload && !forceDisableAutoReload()) {
+            this._autoReloadId = window.setTimeout(() => this._doAutoReload(), this.autoReloadIntervalSecs * 1000);
+        }
+        else {
+            window.clearTimeout(this._autoReloadId);
+        }
     },
-    _doReload: function() {
-      if (this.reload == null) {
-        throw new Error('AutoReloadBehavior requires a reload method');
-      }
-      this.reload();
+    _doAutoReload: function () {
+        if (this._isDocumentVisible()) {
+            this._doReload();
+        }
+        else {
+            this._missedAutoReload = true;
+        }
+        this._autoReloadId = window.setTimeout(() => this._doAutoReload(), this.autoReloadIntervalSecs * 1000);
     },
-    _handleVisibilityChange: function() {
-      if (this._isDocumentVisible() && this._missedAutoReload) {
-        this._missedAutoReload = false;
-        this._doReload();
-      }
+    _doReload: function () {
+        if (this.reload == null) {
+            throw new Error("AutoReloadBehavior requires a reload method");
+        }
+        this.reload();
+    },
+    _handleVisibilityChange: function () {
+        if (this._isDocumentVisible() && this._missedAutoReload) {
+            this._missedAutoReload = false;
+            this._doReload();
+        }
     },
     /**
      * Wraps Page Visibility API call to determine if document is visible.
      * Can be overriden for testing purposes.
      */
-    _isDocumentVisible: function() {
-      return document.visibilityState === 'visible';
+    _isDocumentVisible: function () {
+        return document.visibilityState === "visible";
     },
-  };
-} // namespace tf_tensorboard
+};
---
+++ tensorboard/components_polymer3/tf_tensorboard/plugin-dialog.ts
@@ -0,0 +1,94 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+import { PolymerElement, html } from "@polymer/polymer";
+import { customElement, property } from "@polymer/decorators";
+import { DO_NOT_SUBMIT } from "../tf-imports/polymer.html";
+import { DO_NOT_SUBMIT } from "../tf-imports/lodash.html";
+import { DO_NOT_SUBMIT } from "../tf-imports/polymer.html";
+import { DO_NOT_SUBMIT } from "../tf-imports/lodash.html";
+@customElement("tf-plugin-dialog")
+class TfPluginDialog extends PolymerElement {
+    static readonly template = html `<!-- We use a custom backdrop to avoid occluding the TensorBoard navbar. -->
+    <template is="dom-if" if="[[_open]]">
+      <div id="dashboard-backdrop"></div>
+    </template>
+    <paper-dialog id="dialog" modal="" opened="{{_open}}" with-backdrop="[[_useNativeBackdrop]]">
+      <h2 id="dialog-title">[[_title]]</h2>
+      <div class="custom-message">[[_customMessage]]</div>
+    </paper-dialog>
+    <style>
+      /** We rely on a separate \`_hidden\` property instead of directly making use
+          of the \`_open\` attribute because this CSS specification may strangely
+          affect other elements throughout TensorBoard. See #899. */
+      #dashboard-backdrop {
+        background: rgba(0, 0, 0, 0.6);
+        width: 100%;
+        height: 100%;
+      }
+
+      #dialog-title {
+        padding-bottom: 15px;
+      }
+
+      .custom-message {
+        margin-top: 0;
+        margin-bottom: 15px;
+      }
+    </style>`;
+    @property({
+        type: String
+    })
+    _title: string = null;
+    @property({
+        type: String
+    })
+    _customMessage: string = null;
+    @property({
+        type: Boolean
+    })
+    _open: boolean;
+    @property({
+        type: Boolean,
+        computed: '_computeHidden(_open)',
+        reflectToAttribute: true
+    })
+    _hidden: boolean;
+    @property({
+        type: Boolean,
+        readOnly: true
+    })
+    _useNativeBackdrop: boolean = false;
+    openNoTensorFlowDialog() {
+        this.openDialog('This plugin is disabled without TensorFlow', 'To enable this plugin in TensorBoard, install TensorFlow with ' +
+            '"pip install tensorflow" or equivalent.');
+    }
+    openOldTensorFlowDialog(version) {
+        this.openDialog('This plugin is disabled without TensorFlow ' + version, 'To enable this plugin in TensorBoard, install TensorFlow ' +
+            version +
+            ' or greater with "pip install tensorflow" or equivalent.');
+    }
+    openDialog(title, message) {
+        this.set('_title', title);
+        this.set('_customMessage', message);
+        this.$.dialog.open();
+    }
+    closeDialog() {
+        this.$.dialog.close();
+    }
+    _computeHidden(open) {
+        return !open;
+    }
+}
